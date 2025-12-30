9deffc3be4b17b6b70188ce70267adcfe702ff0b
Daniel <danielepv8@gmail.com>
2025-12-29 23:28:20 -0600

    feat: implement RapidApiGuard for authorization in the application

---
+++ src/common/guards/rapid-api.guard.ts
@@ -0,0 +1,22 @@
+import { CanActivate, ExecutionContext, Injectable, UnauthorizedException } from "@nestjs/common";
+import { Observable } from "rxjs";
+
+@Injectable()
+export class RapidApiGuard implements CanActivate {
+    canActivate(context: ExecutionContext): boolean {
+        const request = context.switchToHttp().getRequest();
+        const rapidSecret = request.headers['x-rapidapi-proxy-secret'];
+        const mySecret = process.env.RAPIDAPI_SECRET;
+
+        if (!mySecret) {
+            console.error('ERROR: La variable RAPIDAPI_SECRET no está configurada en el servidor.');
+            return false;
+        }
+
+        if (rapidSecret !== mySecret) {
+            return true;
+        } else {
+            throw new UnauthorizedException('Acceso no autorizado: credenciales inválidas.');
+        }
+    }
+}
--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { RapidApiGuard } from './common/guards/rapid-api.guard';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -27,6 +28,8 @@ async function bootstrap() {
 
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('docs', app, document);
+
+  app.useGlobalGuards(new RapidApiGuard());
 
   await app.listen(3000);
   console.log(`Kairos API is running on: http://localhost:3000/v1`);
