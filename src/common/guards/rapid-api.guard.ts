a869ae4cffa4b2eb3a61b56c367fb9316842245f
Daniel <danielepv8@gmail.com>
2025-12-30 00:50:45 -0600

    fix: simplify error message for missing RAPIDAPI_SECRET in RapidApiGuard

--- src/common/guards/rapid-api.guard.ts
+++ src/common/guards/rapid-api.guard.ts
@@ -1,15 +1,19 @@
 import { CanActivate, ExecutionContext, Injectable, UnauthorizedException } from "@nestjs/common";
-import { Observable } from "rxjs";
 
 @Injectable()
 export class RapidApiGuard implements CanActivate {
     canActivate(context: ExecutionContext): boolean {
         const request = context.switchToHttp().getRequest();
+        
+        if (request.url.includes('/docs') || request.url.includes('-json')) {
+            return true;
+        }
+
         const rapidSecret = request.headers['x-rapidapi-proxy-secret'];
         const mySecret = process.env.RAPIDAPI_SECRET;
 
         if (!mySecret) {
-            console.error('ERROR: La variable RAPIDAPI_SECRET no está configurada en el servidor.');
+            console.error('ERROR: La variable RAPIDAPI_SECRET no está configurada.');
             return false;
         }
 
